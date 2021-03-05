8a58723b03d4597c1e8a93e3ef63dddcfb991802
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-03-05 18:47:21 -0300

    chore: project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/robsonlopesjr.png" alt="Robson Júnior"/>
+            <div>
+                <strong>Robson Júnior</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
